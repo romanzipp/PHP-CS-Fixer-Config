024efdaf7a075d11c398a2139c6ca9123faad2bf
Roman Zipp <ich@ich.wtf>
2021-01-27 15:01:29 +0100

    Update rules doc urls

--- src/Presets/PrettyPHP.php
+++ src/Presets/PrettyPHP.php
@@ -7,20 +7,20 @@ class PrettyPHP extends AbstractPreset
     public function getRules(): array
     {
         return [
-            '@Symfony' => true,                         // https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/2.17/doc/ruleSets/Symfony.rst
-            'array_indentation' => true,                // https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/2.17/doc/rules/whitespace/array_indentation.rst
+            '@Symfony' => true,                         // https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/doc/ruleSets/Symfony.rst
+            'array_indentation' => true,                // https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/doc/rules/whitespace/array_indentation.rst
             // see https://github.com/FriendsOfPHP/PHP-CS-Fixer/issues/4487
-            'braces' => true,                           // https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/2.17/doc/rules/basic/braces.rst
-            'concat_space' => [                         // https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/2.17/doc/rules/operator/concat_space.rst
+            'braces' => true,                           // https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/doc/rules/basic/braces.rst
+            'concat_space' => [                         // https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/doc/rules/operator/concat_space.rst
                 'spacing' => 'one',
             ],
-            'no_superfluous_phpdoc_tags' => false,      // https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/2.17/doc/rules/phpdoc/no_superfluous_phpdoc_tags.rst
-            'no_useless_return' => true,                // https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/2.17/doc/rules/return_notation/no_useless_return.rst
-            'not_operator_with_space' => true,          // https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/2.17/doc/rules/operator/not_operator_with_space.rst
-            'phpdoc_align' => [                         // https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/2.17/doc/rules/phpdoc/phpdoc_align.rst
+            'no_superfluous_phpdoc_tags' => false,      // https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/doc/rules/phpdoc/no_superfluous_phpdoc_tags.rst
+            'no_useless_return' => true,                // https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/doc/rules/return_notation/no_useless_return.rst
+            'not_operator_with_space' => true,          // https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/doc/rules/operator/not_operator_with_space.rst
+            'phpdoc_align' => [                         // https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/doc/rules/phpdoc/phpdoc_align.rst
                 'align' => 'left',
             ],
-            'phpdoc_return_self_reference' => [         // https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/2.17/doc/rules/phpdoc/phpdoc_return_self_reference.rst
+            'phpdoc_return_self_reference' => [         // https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/doc/rules/phpdoc/phpdoc_return_self_reference.rst
                 'this' => 'self',
                 '@this' => 'self',
                 '$self' => 'self',
@@ -28,8 +28,8 @@ class PrettyPHP extends AbstractPreset
                 '$static' => 'static',
                 '@static' => 'static',
             ],
-            'phpdoc_order' => true,                     // https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/2.17/doc/rules/phpdoc/phpdoc_order.rst
-            'phpdoc_to_comment' => false,               // https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/2.17/doc/rules/phpdoc/phpdoc_to_comment.rst
+            'phpdoc_order' => true,                     // https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/doc/rules/phpdoc/phpdoc_order.rst
+            'phpdoc_to_comment' => false,               // https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/doc/rules/phpdoc/phpdoc_to_comment.rst
         ];
     }
 
